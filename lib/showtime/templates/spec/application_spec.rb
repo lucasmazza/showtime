957dfeb64ba2dcfe524a103b3894d028b8e94798
Lucas Mazza <luc4smazza@gmail.com>
2010-07-16 19:47:41 -0300

    fixed sample spec

--- lib/showtime/templates/spec/application_spec.rb
+++ lib/showtime/templates/spec/application_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Sinatra::Application do
   context "responding to GET /" do
-    it "should return status code 200"
+    it "should return status code 200" do
       get '/'
-      last_response.code.should == 200
+      last_response.status.should == 200
     end
   end
 end
