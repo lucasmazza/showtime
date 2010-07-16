cda386060a996206c56b8ce894e6a6989b4e0019
Lucas Mazza <luc4smazza@gmail.com>
2010-07-16 18:58:14 -0300

    Yay, basic generator up and running

--- lib/showtime/generator.rb
+++ lib/showtime/generator.rb
@@ -1,10 +1,29 @@
 module Showtime
   class Generator < Thor::Group
     include Thor::Actions
-        
+    
+    argument :name, :optional => true, :default => 'application'
+    
     def self.source_root
       File.dirname(__FILE__)
     end
     
+    def create_application_file
+      template("templates/application.rb", "#{name}/application.rb")
+    end
+    def create_rakefile
+      template("templates/Rakefile", "#{name}/Rakefile")
+    end
+    def create_config_ru
+      template("templates/config.ru", "#{name}/config.ru")
+    end
+    
+    def create_lib_folder
+      empty_directory("#{name}/lib")
+    end
+
+    def create_spec_folder_and_helper
+      directory("templates/spec")
+    end
   end
 end
